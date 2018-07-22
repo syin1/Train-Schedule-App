c78a915a3d8db445c3de565996bf068e4be26697
syin <shaomingyinut@gmail.com>
2018-07-22 06:57:19 -0400

    implemented remove functionality

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -53,45 +53,75 @@ $(document).ready(function() {
     }
   });
 
+  function updateList(snapshot) {
+    var obj = snapshot.val();
+
+    var firsttraintime = moment(obj.firsttraintime, 'HH:mm');
+    var current = moment();
+    var diff = current.diff(firsttraintime, 'minutes');
+    var nextArrival;
+
+    var frequency = parseInt(obj.frequency);
+
+    if (diff <= 0) {
+      nextArrival = firsttraintime.format('hh:mm a');
+    } else if (diff % frequency === 0) {
+      nextArrival = current.format('hh:mm a');
+    } else {
+      nextArrival = firsttraintime
+        .add(Math.ceil(diff / frequency) * frequency, 'minutes')
+        .format('hh:mm a');
+    }
+
+    var minutesAway = moment(nextArrival, 'hh:mm a').diff(
+      current.startOf('minute'),
+      'minutes'
+    );
+
+    var row = $("<tr id='" + snapshot.key + "'>");
+
+    row.append($('<td>').text(obj.trainname));
+    row.append($('<td>').text(obj.destination));
+    row.append($('<td>').text(frequency));
+    row.append($('<td>').text(nextArrival));
+    row.append($('<td>').text(minutesAway));
+    row.append(
+      $('<td>').html(
+        "<button type='button' data-key='" +
+          snapshot.key +
+          "' class='btn btn-outline-danger remove'>Remove</button><span> </span><button type='button' data-key='" +
+          snapshot.key +
+          "' class='btn btn-outline-warning update'>Update</button>"
+      )
+    );
+
+    $('#tabledisplay').append(row);
+  }
+
   database.ref().on(
     'child_added',
     function(snapshot) {
-      var obj = snapshot.val();
-
-      var firsttraintime = moment(obj.firsttraintime, 'HH:mm');
-      var current = moment();
-      var diff = current.diff(firsttraintime, 'minutes');
-      var nextArrival;
-
-      var frequency = parseInt(obj.frequency);
-
-      if (diff <= 0) {
-        nextArrival = firsttraintime.format('hh:mm a');
-      } else if (diff % frequency === 0) {
-        nextArrival = current.format('hh:mm a');
-      } else {
-        nextArrival = firsttraintime
-          .add(Math.ceil(diff / frequency) * frequency, 'minutes')
-          .format('hh:mm a');
-      }
-
-      var minutesAway = moment(nextArrival, 'hh:mm a').diff(
-        current.startOf('minute'),
-        'minutes'
-      );
-
-      var row = $('<tr>');
-
-      row.append($('<td>').text(obj.trainname));
-      row.append($('<td>').text(obj.destination));
-      row.append($('<td>').text(frequency));
-      row.append($('<td>').text(nextArrival));
-      row.append($('<td>').text(minutesAway));
-
-      $('#tabledisplay').append(row);
+      updateList(snapshot);
     },
     function(errorObject) {
       console.log('Error: ' + errorObject.code);
     }
   );
+
+  database.ref().on(
+    'child_removed',
+    function(snapshot) {
+      $('#' + snapshot.key).remove();
+    },
+    function(errorObject) {
+      console.log('Error: ' + errorObject.code);
+    }
+  );
+
+  $(document.body).on('click', '.remove', function() {
+    database
+      .ref()
+      .child($(this).attr('data-key'))
+      .remove();
+  });
 });
